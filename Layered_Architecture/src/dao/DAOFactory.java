4982810d592142632b6635aeab4462f6d7166ae6
KasunKVI <kkhettiarachchi10@gmail.com>
2023-05-26 15:57:32 +0530

    DAOFactory and SuperDAO

---
+++ Layered_Architecture/src/dao/DAOFactory.java
@@ -0,0 +1,39 @@
+package dao;
+
+import dao.custom.impl.*;
+
+public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory == null) ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+
+        CUSTOMER,ITEM,ORDER,ORDER_DETAIL,QUERY_DAO
+
+    }
+
+    public SuperDAO getDAO(DAOTypes types){
+
+        switch (types){
+
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            case ITEM:
+                return new ItemDAOImpl();
+            case ORDER:
+                return new OrderDAOImpl();
+            case ORDER_DETAIL:
+                return new OrderDetailDAOImpl();
+            case QUERY_DAO:
+                return new QueryDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
